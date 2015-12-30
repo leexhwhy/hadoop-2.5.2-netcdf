915fdc57a929339db27a2bade16b2367722e216d
sbioo001 <sbioo001@fiu.edu>
2015-12-29 18:31:27 -0700

    Message #117

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/output/NetCDFOutputFormat.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/output/NetCDFOutputFormat.java
@@ -172,12 +172,12 @@ public class NetCDFOutputFormat<Text, NetCDFArrayWritable> extends FileOutputFor
                 outputFile.addGroupAttribute(null, new Attribute("modeling_realm", "atmos"));
                 outputFile.addGroupAttribute(null, new Attribute("cmor_version", "2.8.3"));
 
-                ArrayDouble.D1 latArray = (ArrayDouble.D1) vlat.read();
-                Array dataLat = Array.factory(DataType.DOUBLE, new int[]{1});
-                int[] shape = latArray.getShape();
-                for( int i = 0; i < shape[0]; i++ ){
-                    dataLat.setDouble(Integer.valueOf(currentLat), latArray.get(i));
-                }
+                //ArrayDouble.D1 latArray = (ArrayDouble.D1) vlat.read();
+                //Array dataLat = Array.factory(DataType.DOUBLE, new int[]{1});
+                int[] shape;
+                //for( int i = 0; i < shape[0]; i++ ){
+                dataLat.setDouble(0, Double.valueOf(currentLat));
+                //}
 
                 ArrayDouble.D2 latBndsArray = (ArrayDouble.D2) vlat_bnds.read();
                 Array dataLatBnds = Array.factory(DataType.DOUBLE, new int[]{(int)(vlat.getSize()), 2});
