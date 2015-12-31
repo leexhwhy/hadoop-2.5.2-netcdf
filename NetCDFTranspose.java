2c7635c77abda92de962e2b9abb13a38927dd49b
sbioo001 <sbioo001@fiu.edu>
2015-12-30 21:09:43 -0700

    Message #130

--- NetCDFTranspose.java
+++ NetCDFTranspose.java
@@ -107,8 +107,8 @@ public class NetCDFTranspose {
                 int lonIndex = Integer.valueOf(valueParts[1]);
                 System.out.println( "[SAMAN][NetCDFTranspose][Reducer] set index("+timeIndex
                         +","+Integer.valueOf(dimensions[0])+","+lonIndex+") with value="+valueParts[2]
-                        +", and fw index of " + (timeIndex*timeDim+lonDim));
-                fw[timeIndex*timeDim+lonDim] = new FloatWritable(Float.valueOf(valueParts[2]));
+                        +", and fw index of " + (timeIndex*timeDim+lonIndex);
+                fw[timeIndex*timeDim+lonIndex] = new FloatWritable(Float.valueOf(valueParts[2]));
             }
 
             result.set( fw );
