1e7edfbf3511ca6befc007c04ef26f8b1bd1a5e6
saman <saman.aghazadeh@gmail.com>
2016-01-08 13:09:38 -0700

    #220

--- NetCDFTransposeCompact2.java
+++ NetCDFTransposeCompact2.java
@@ -241,7 +241,7 @@ public class NetCDFTransposeCompact2 {
         job.setMapOutputValueClass(NetCDFArrayWritable.class);
         job.setOutputValueClass(Key.class);
         job.setInputFormatClass(NetCDFInputFormatWithDimensions.class);
-        job.setOutputFormatClass(NetCDFOutputFormatCompact.class);
+        job.setOutputFormatClass(NetCDFOutputFormatCompact2.class);
         job.setNumReduceTasks(2);
         String singleInput = otherArgs[0];
         conf.set( NetCDFOutputFormat.NETCDF_INPUT_PATH, singleInput );
