55fe5407c1b9db41d52bc9b7b4dd6a5020a62504
saman <saman.aghazadeh@gmail.com>
2016-02-08 12:38:40 -0700

    #15

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/output/NetCDFOutputFormatCompactForTime.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/output/NetCDFOutputFormatCompactForTime.java
@@ -321,7 +321,7 @@ public class NetCDFOutputFormatCompactForTime<Text, List> extends FileOutputForm
                 //long first2 = System.nanoTime();
 
                 //System.out.println("[SAMAN][NetCDFOutputFormat][Write] Before DataRsut;");
-                Index3D idx3 = new Index3D(new int[]{latIndexesSize, (int) (vlat.getSize()), (int) (vlon.getSize())});
+                Index3D idx3 = new Index3D(new int[]{timeIndexesSize, (int) (vlat.getSize()), (int) (vlon.getSize())});
                 Array dataRsut = Array.factory(DataType.FLOAT, new int[]{timeIndexesSize, (int) (vlat.getSize()), (int) (vlon.getSize())});
                 int globalIndex = 0;
 
