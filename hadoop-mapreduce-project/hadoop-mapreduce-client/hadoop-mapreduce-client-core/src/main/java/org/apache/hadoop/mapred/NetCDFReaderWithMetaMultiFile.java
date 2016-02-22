cc1bc9a8885943f5344d5bb63ec4cafafec5b914
saman <saman.aghazadeh@gmail.com>
2016-02-22 16:11:56 -0700

    #31

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/NetCDFReaderWithMetaMultiFile.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/NetCDFReaderWithMetaMultiFile.java
@@ -51,26 +51,37 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
     private static final Log LOG
             = LogFactory.getLog(LineRecordReader.class.getName());
 
-    private long start;
+    private List<Long> start;
+
+    private List<Long> end;
+    private List<NetcdfFile> ncFile;
+    private List<Variable> v;
     private long pos;
-    private long end;
-    private NetcdfFile ncFile;
-    private Variable v;
+    private int numberOfElements;
+
+    int currChunk = 0;
+
 
     public NetCDFReaderWithMetaMultiFile(Configuration job,
                                 NetCDFFileSplit split) throws IOException {
-        start = split.getFileSplit().startChunk.get(0); //split.getStart();
-        end = split.getFileSplit().endChunk.get(0); //start + split.getLength();
-        final Path file = split.getPath();
 
-        LOG.info("Map is reading from input: " + file +" start chunk "+ start+" end chunk "+end);
 
-        ncFile = NetcdfDataset.openFile(file.toString(), null);
-        List<Variable> vs = ncFile.getVariables();
-        v = vs.get(vs.size()-1);
-        //LOG.info("Variable is "+ v.getFullName());
+        numberOfElements = split.getFileSplit().getPaths().size();
+        for( int i = 0; i < numberOfElements; i++ ) {
 
-        this.pos = start;
+            start.add(split.getFileSplit().startChunk.get(i)); //split.getStart();
+            end.add(split.getFileSplit().endChunk.get(0)); //start + split.getLength();
+            final Path file = split.getFileSplit().getPaths().get(i);
+
+            LOG.info("Map is reading from input: " + file + " start chunk " + start + " end chunk " + end);
+
+            ncFile.add(NetcdfDataset.openFile(file.toString(), null));
+            List<Variable> vs = ncFile.getVariables();
+            v.add(vs.get(vs.size() - 1));
+            //LOG.info("Variable is "+ v.getFullName());
+
+        }
+        this.pos = start.get(0);
     }
 
 
@@ -86,14 +97,15 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
     public synchronized boolean next(Text key, NetCDFArrayWritable value)
             throws IOException {
 
-        if (pos < end) {
+
+        if (pos < end.get(currChunk)) {
             key.set(String.valueOf(pos));
             Array chunk = null;
             try{
 
                 long first = System.currentTimeMillis();
 
-                chunk = ncFile.readSection("rsut("+pos+":"+pos+",:,:)");
+                chunk = ncFile.get(currChunk).readSection("rsut(" + pos + ":" + pos + ",:,:)");
 
                 long second = System.currentTimeMillis();
                 LOG.info( "[SAMAN][NetCDFReaderWithMeta][Next] read time = " + (second - first) );
@@ -105,7 +117,7 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
             if (chunk == null) {LOG.info("chunk is null");return false;}
             LOG.info(chunk.getSize()+" elements and "+chunk.getSizeBytes()+" bytes, shape is "+Arrays.toString(chunk.getShape()));
             System.out.println(chunk.getSize()+" elements and "+chunk.getSizeBytes()+" bytes, shape is "+Arrays.toString(chunk.getShape()));
-            int dimensionsSize = v.getDimensions().size();
+            int dimensionsSize = v.get(currChunk).getDimensions().size();
 
             long third = System.currentTimeMillis();
 
@@ -119,7 +131,7 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
             fw[0] = new FloatWritable( dimensionsSize );
             fw[1] = new FloatWritable( pos );
             for (int i = 2; i < dimensionsSize+1; i++ ){
-                fw[i] = new FloatWritable(v.getDimensions().get(i-1).getLength());
+                fw[i] = new FloatWritable(v.get(currChunk).getDimensions().get(i-1).getLength());
             }
             for (int i=dimensionsSize+1; i< my.length+dimensionsSize+1; i++) {
                 fw[i]=new FloatWritable(my[i-dimensionsSize-1]);
@@ -129,6 +141,14 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
 
             value.set(fw);
             pos ++;
+
+            if( pos >= end.get(currChunk) ){
+                if( currChunk < numberOfElements-1 ){
+                    currChunk++;
+                    pos = start.get(currChunk);
+                }
+            }
+
             return true;
 
         }
@@ -141,11 +161,14 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
      * Get the progress within the split
      */
     public float getProgress() {
+        return 0.0f;
+        /*
         if (start == end) {
             return 0.0f;
         } else {
             return Math.min(1.0f, (pos - start) / (float)(end - start));
         }
+        */
     }
 
     public  synchronized long getPos() throws IOException {
@@ -153,8 +176,11 @@ public class NetCDFReaderWithMetaMultiFile implements RecordReader<Text, NetCDFA
     }
 
     public synchronized void close() throws IOException {
-        if (ncFile != null) {
-            ncFile.close();
+
+        for( int i = 0; i < numberOfElements; i++ ) {
+            if (ncFile.get(i) != null) {
+                ncFile.get(i).close();
+            }
         }
     }
 }
