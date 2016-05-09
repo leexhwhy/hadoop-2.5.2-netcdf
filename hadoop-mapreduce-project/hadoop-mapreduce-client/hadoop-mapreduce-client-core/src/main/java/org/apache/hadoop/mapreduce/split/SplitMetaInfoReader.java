eae5877cc2818e8dcf420bf41de58271179cf263
saman <saman.aghazadeh@gmail.com>
2016-05-09 13:19:56 -0700

    #30

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/split/SplitMetaInfoReader.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/split/SplitMetaInfoReader.java
@@ -109,10 +109,12 @@ public class SplitMetaInfoReader {
       // read forcefully from a remote node
       // We would consider values 1/4, 1/2, and full
 
-      System.out.println( "[SAMAN][SplitMetaInfoReader][readSplitMetaInfo] locations = " +
-              splitMetaInfo.getLocations()[0] + "," +
-              splitMetaInfo.getLocations()[1] + "," +
-              splitMetaInfo.getLocations()[2] );
+      if( splitMetaInfo.getLocations().length == 3 ) {
+        System.out.println("[SAMAN][SplitMetaInfoReader][readSplitMetaInfo] locations = " +
+                splitMetaInfo.getLocations()[0] + "," +
+                splitMetaInfo.getLocations()[1] + "," +
+                splitMetaInfo.getLocations()[2]);
+      }
 
       if( secondBestLayoutEnabled && isNetCDF ){
         String[] reorderedLocations = new String[3];
