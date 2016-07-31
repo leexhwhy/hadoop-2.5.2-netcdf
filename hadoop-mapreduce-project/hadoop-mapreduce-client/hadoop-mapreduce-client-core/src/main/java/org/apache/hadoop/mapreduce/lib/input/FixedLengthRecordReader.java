ee784c18d91e47f8a725c0f83d85aed15e55759e
sbioo001 <sbioo001@fiu.edu>
2016-07-30 20:17:41 -0700

    -

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/input/FixedLengthRecordReader.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/input/FixedLengthRecordReader.java
@@ -18,9 +18,8 @@
 
 package org.apache.hadoop.mapreduce.lib.input;
 
-import java.io.IOException;
-import java.io.InputStream;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
@@ -30,16 +29,13 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.Seekable;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.compress.CodecPool;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
-import org.apache.hadoop.io.compress.CompressionInputStream;
-import org.apache.hadoop.io.compress.Decompressor;
+import org.apache.hadoop.io.compress.*;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A reader to read fixed length records from a split.  Record offset is
@@ -158,8 +154,9 @@ public class FixedLengthRecordReader
             numRecordsRemainingInSplit--;
           }
         } else {
-          throw new IOException("Partial record(length = " + numBytesRead
-              + ") found at the end of split.");
+          //throw new IOException("Partial record(length = " + numBytesRead
+          //    + ") found at the end of split.");
+          numRecordsRemainingInSplit = 0L;
         }
       } else {
         numRecordsRemainingInSplit = 0L; // End of input.
