c2eff952b03ef408f275365c6daff4bc7beebe0a
sbioo001 <sbioo001@fiu.edu>
2016-07-30 19:54:44 -0700

    --

--- FixedLength.java
+++ FixedLength.java
@@ -21,7 +21,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-impotr org.apache.hadoop.io.ByteWritable;
+import org.apache.hadoop.io.ByteWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
